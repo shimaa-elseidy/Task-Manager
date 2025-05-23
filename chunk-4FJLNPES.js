7a91524054b1f14f49279e3261eb99e5eb3278ec
Shimaa El-seidy <shimaaelseidy123@gmail.com>
2025-05-23 23:46:37 +0300

    TODO

---
+++ chunk-4FJLNPES.js
@@ -0,0 +1,1 @@
+import{Pa as o,Qa as a,X as n,Xa as i,cb as r}from"./chunk-JKWB2DPL.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-sidebar"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,m){e&1&&(o(0,"p"),i(1,"sidebar works!"),a())}})};export{s as SidebarComponent};
---
+++ chunk-Z5DECTUN.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-ZQD3PHQI.js";import"./chunk-JKWB2DPL.js";export{a as NavbarComponent};
